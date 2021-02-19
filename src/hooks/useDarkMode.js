c8310eee7735a82096deb1e8d7ab62afa07eb92c
PrinceIwejuo <princeiwejuo@gmail.com>
2021-02-19 10:36:58 -0500

    DarkModeProject

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import useLocalStorage from "./useLocalStorage"
+
+export const useDarkMode = (value) => {
+    const [ darkMode, setDarkMode ] = useLocalStorage(value)
+
+    return [ darkMode, setDarkMode ]
+
+};
